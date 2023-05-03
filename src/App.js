163112172452771f89045a8dbe46123dfcd3cbda
krushnagaikwad07 <krushnagaikwad7799@gmail.com>
2023-05-03 11:35:25 +0530

    All done for searchable-dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import SearchableDropdown from './SearchableDropdown';
 
-function App() {
+export default function App() {
+  const options = ['Cricket', 'Bat', 'Ball', 'Football', 'Khokho', 'Kabaddi', 'Swimming','Dancing'];
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <SearchableDropdown options={options} />
+
+  </div>
   );
 }
-
-export default App;
